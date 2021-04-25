fce1d842322dd4dc6a6f72963dee236e8ef0d713
Etrix06 <82925150+Etrix06@users.noreply.github.com>
2021-04-25 08:03:48 -0700

    Update random.js

--- random.js
+++ random.js
@@ -1,4 +1,33 @@
-const links = ["Week1", "Week2", "Week3"];
+const links = [
+  {
+     label: "Week1 notes",
+     url: "week1/week1index.html"
+  },
+  {  label: "Week2 notes",
+     url: "week2/week2index.html"
+     
+  },
+  {
+     label: "Week3 notes",
+     url: "week3/week3index.html"
+  },
+  {
+    label: "Week4 notes",
+     url: "week4/week4index.html"   
+  },
+  {
+     label: "Week5 notes",
+     url: "week5/week5index.html"  
+  },
+  {
+     label: "Week6 notes",
+     url: "week6/week6index.html"  
+  },
+  {
+     label: "Week7 notes",
+     url: "week7/week7index.html"  
+  }
+  ];
 
 function testFunction () {
    document.getElementById("testid").innerHTML= links;
