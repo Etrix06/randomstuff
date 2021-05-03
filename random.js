82f023cd73f64f9e87f08cb508d7be7a3ef7d349
Etrix06 <82925150+Etrix06@users.noreply.github.com>
2021-05-02 21:32:34 -0700

    Lots of changes

--- random.js
+++ random.js
@@ -1,21 +1,20 @@
 const links = [
-     {  label: "Week 1",        url: "week1/week1index"    },
+     {  label: "NEGL Page",        url: "NEGL/mainIndex.html"    },
      {  label: "Week 2",        url: "week2/week2index"    }
      
 ]
 
-function pageLoad () {
-  
-  //Create the element
-  var a = document.createElement("a"); 
-  var linkText = document.createTextNode("Week 1");
-  a.appendChild(linkText);
-  a.title = "This is the week 1 Link";
-  a.href = "week1/week1index.html";
-
-  var y = document.createElement("LI");
-  y.appendChild(a);
-  document.getElementById("dynamic").appendChild(y);
+function addToIndex() {
+    for (let i = 0; i < links.length; i++ ) {
+      var a = document.createElement("a");
+      var linkText = document.createTextNode(links[i].label);
+      a.appendChild(linkText);
+      a.title = "This is the " + links[i].label + " link";
+      a.href = links[i].url;
+      var y = document.createElement("LI");
+      y.appendChild(a);
+      document.getElementById("indexList").appendChild(y);
+    }
 }
 
 function testFunction () {
