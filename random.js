a55ee7c0eb4220e4723542f74848298601c88620
Etrix06 <82925150+Etrix06@users.noreply.github.com>
2021-05-04 20:11:52 -0700

    Update random.js

--- random.js
+++ random.js
@@ -3,6 +3,38 @@ const links = [
      {  label: "Week 2",        url: "week2/week2index"    }
      
 ]
+const superman = {
+    name: 'Superman',
+    'real name': 'Clark Kent',
+    height: 75,
+    weight: 235,
+    hero: true,
+    villain: false,
+    allies: ['Batman','Supergirl','Superboy'],
+    fly() {
+        return 'Up, up and away!';
+    }
+};
+console.log(superman.fly());
+console.log(superman.name);
+console.log(superman['weight']);
+console.log(superman['real name']);
+console.log(superman.allies[1]);
+console.log('city' in superman);  //is city in superman object.
+console.log(superman.city !== undefined);
+console.log(superman.hasOwnProperty('name'));
+console.log();
+for(const key in superman) {
+    console.log(key + ": " + superman[key]);
+}
+
+console.log(superman);
+
+for(const key in superman) {
+    if(superman.hasOwnProperty(key)){
+        console.log(key + ": " + superman[key]);
+    }
+}
 
 function addToIndex() {
     for (let i = 0; i < links.length; i++ ) {
